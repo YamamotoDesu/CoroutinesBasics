09b5b5d1804d11b9f54780f9745eba8455dcb781
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-08-03 23:51:58 +0900

    Dispatchers

---
+++ src/main/kotlin/Dispatchers.kt
@@ -0,0 +1,35 @@
+import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+//        launch(Dispatchers.Main) {
+//            println("Main dispatcher. Thread: ${Thread.currentThread().name}")
+//        }
+
+        launch(Dispatchers.Unconfined) {
+            println("Unconfined1. Thread: ${Thread.currentThread().name}")
+            delay(100L)
+            println("Unconfined2. Thread: ${Thread.currentThread().name}")
+        }
+
+        launch(Dispatchers.Default) {
+            println("Default. Thread: ${Thread.currentThread().name}")
+        }
+
+        launch(Dispatchers.IO) {
+            println("IO. Thread:: ${Thread.currentThread().name}")
+        }
+
+        launch(newSingleThreadContext("MyThread")) {
+            println("newSingleThreadContext. Thread: ${Thread.currentThread().name}")
+        }
+
+        /*
+        Unconfined1. Thread: main
+        Default. Thread: DefaultDispatcher-worker-1
+        IO. Thread:: DefaultDispatcher-worker-2
+        Unconfined2. Thread: kotlinx.coroutines.DefaultExecutor
+        newSingleThreadContext. Thread: MyThread
+         */
+    }
+}
